ce603b664a7f94ac0902011730194341ca2a11bc
chlwnstjr94 <chlwnstjr940404@gmail.com>
2022-04-21 17:03:52 +0900

    feat: Add beanist class

---
+++ asset/js/swiper.js
@@ -0,0 +1,27 @@
+new Swiper(".swiper.visual", {
+  loop: true,
+  speed: 400,
+  centeredSlides: true,
+  autoplay: {
+    delay: 4000,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+new Swiper(".swiper.beanist", {
+  loop: true,
+  speed: 400,
+  centeredSlides: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
