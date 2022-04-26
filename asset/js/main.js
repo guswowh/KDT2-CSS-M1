bfb4175b9f3134c5e11fea1fc8406efd693a8d33
chlwnstjr94 <chlwnstjr940404@gmail.com>
2022-04-26 13:38:22 +0900

    feat: Add click event & resize event

--- asset/js/main.js
+++ asset/js/main.js
@@ -2,3 +2,61 @@ window.addEventListener('scroll',function() {
   const header = document.querySelector('header');
   header.classList.toggle('sticky',scrollY > 50);
 });
+
+document.querySelector('.menu-btn').addEventListener('click', () => {
+  if (document.querySelector('.menu-wrap').className !== 'active') {
+    document.querySelector('.menu-wrap').classList.add('active');
+  }
+});
+
+function closeBtn() {
+  document.querySelector('.menu-wrap').classList.remove('active');
+}
+
+if (window.innerWidth <= 1280) {
+  document.querySelector('.header-nav').classList.remove('hover');
+  document.querySelectorAll('.header-nav > ul > li').forEach(item => {
+    item.classList.remove('hover');
+  });
+  document.querySelectorAll('.header-nav_contents > li > a').forEach(item => {
+    item.classList.remove('hover');
+  });
+} else {
+  document.querySelector('.header-nav').classList.add('hover');
+  document.querySelectorAll('.header-nav > ul > li').forEach(item => {
+    item.classList.add('hover');
+  });
+  document.querySelectorAll('.header-nav_contents > li > a').forEach(item => {
+    item.classList.add('hover');
+  });
+}
+
+window.addEventListener('resize', function() {
+  if (window.innerWidth <= 1280) {
+    document.querySelector('.header-nav').classList.remove('hover');
+    document.querySelectorAll('.header-nav > ul > li').forEach(item => {
+      item.classList.remove('hover');
+    });
+    document.querySelectorAll('.header-nav_contents > li > a').forEach(item => {
+      item.classList.remove('hover');
+    });
+  } else {
+    document.querySelector('.header-nav').classList.add('hover');
+    document.querySelectorAll('.header-nav > ul > li').forEach(item => {
+      item.classList.add('hover');
+    });
+    document.querySelectorAll('.header-nav_contents > li > a').forEach(item => {
+      item.classList.add('hover');
+    });
+  }
+})
+
+document.querySelectorAll('.header-nav > ul > li').forEach((item) => {
+  item.addEventListener('click', function() {
+    if (this.className !== 'active') {
+      this.classList.add('active');
+    } else {
+      this.classList.remove('active');
+    }
+  })
+})
