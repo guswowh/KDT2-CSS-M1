e97a91a7eee6dc43dc5dd43e4183de35f7840448
S-MunSoo <godmsoo93@gmail.com>
2022-04-27 14:49:45 +0900

    클론 코딩 과제제출

---
+++ main.js
@@ -0,0 +1,7 @@
+// const imgConEls = document.querySelector(".img__box");
+
+// for (let i = 0; i < imgConEls.length; i += 1) {
+//   imgConEls[i].addEventListener("click", function () {
+//     imgConEls[i].classList.toggle("--focus");
+//   });
+// }
